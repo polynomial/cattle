063d0c8709ac73b22a4f082a4257e28aff7471a8
Ben Smith <polynomial@gmail.com>
2017-04-29 12:08:19 -0700

    sp3 configuration

---
+++ nixos/surface.nix
@@ -0,0 +1,96 @@
+{ config, pkgs, ... }:
+rec {
+  services.xserver.synaptics = {
+    enable = true;
+    twoFingerScroll = true;
+    palmDetect = false;
+    buttonsMap = [ 1 3 2 ];
+    fingersMap = [ 1 3 2 ];
+    minSpeed = "0.8";
+    maxSpeed = "1.4";
+    additionalOptions = ''
+    MatchDevicePath "/dev/input/event*"
+    Option "vendor" "045e"
+    Option "product" "07e2"
+    '';
+  };
+
+  boot.kernelModules = [ "hid-multitouch" ];
+  boot.initrd.kernelModules = [ "hid-multitouch" ];
+  boot.loader.grub.enable = false;
+  #boot.loader.gummiboot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  hardware.pulseaudio.enable = true;
+  hardware.bluetooth.enable = true;
+
+  #suspend if powerbutton his bumped, rather than shutdown.
+  services.logind.extraConfig = ''
+    HandlePowerKey=suspend
+    HandleLidSwitch=ignore
+  '';
+
+  powerManagement.enable = true;
+  #powerManagement.powerUpCommands = "";
+  #powerManagement.powerDwnCommands = "";
+  powerManagement.cpuFreqGovernor = "powersave";
+
+#  boot.kernelPackages = pkgs.linuxPackages_4_1;
+#  nixpkgs.config.packageOverrides = pkgs: {
+#    linux_4_1 = pkgs.linux_4_1.override {
+#      kernelPatches = [
+#        { patch = ./Add-multitouch-support-for-Microsoft-Type-Cover-3.patch;
+#          name = "multitouch-type-cover-3";
+#          extraConfig = ''
+#            I2C_DESIGNWARE_PLATFORM m
+#            X86_INTEL_LPSS y
+#          ''; }
+#        { patch = ./Add-Microsoft-Surface-Pro-3-button-support.patch; name = "sp3-buttons"; }
+#        { patch = ./Add-Microsoft-Surface-Pro-3-camera-support.patch; name = "surfacepro3-cameras"; }
+#      ];
+#    };
+#  };
+
+  systemd.timers.lidcheck = {
+    partOf = [ "acpid.service"];
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnUnitActiveSec = "60";
+      OnBootSec = "60";
+    };
+  };
+  systemd.services.lidcheck = {
+    environment = { DISPLAY = ":0"; };
+    description = "ensure sleep when unpowered and lid closed";
+    script = services.acpid.lidEventCommands;
+  };
+
+  services.acpid.enable = true;
+  #on lid event, lock if the lid is closed and we have power;
+  # if closed and no power, sleep
+
+  services.acpid.lidEventCommands = ''
+    export PATH=/run/current-system/sw/bin
+    LID_STATE=$(awk '{ print $2 }' /proc/acpi/button/lid/LID0/state)
+    AC_STATE=$(cat /sys/class/power_supply/AC0/online)
+    export DISPLAY=':0'
+    if [ $LID_STATE = 'closed' ]; then
+      xset dpms force off
+      #xautolock -locknow
+      if [ $AC_STATE = '0' ]; then
+        systemctl suspend
+      fi
+    fi
+
+  '';
+
+  services.acpid.acEventCommands = ''
+    export PATH=/run/current-system/sw/bin
+    AC_STATE=$(cat /sys/class/power_supply/AC0/online)
+    #if [ $AC_STATE = '0' ]; then
+    #  iw dev wlp1s0 set power_save on
+    #else
+    #  iw dev wlp1s0 set power_save off
+    #fi
+  '';
+}
